c7843004fb94e094d7947b089b8d29b35c356e69
Pustai Bogdan <pustaibogdan16@gamil.com>
2020-09-25 23:48:40 +0300

    updATEIN whit shop page collections

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import HomePage from './pages/homepage.component'
-
+import HomePage from './pages/homepage/homepage.component';
+import {Switch,Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component.jsx';
 import './App.css';
+
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path={'/'} component={HomePage}/>
+        <Route path={'/shop'} component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
